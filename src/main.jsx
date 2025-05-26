c6914d049222913588762e6fdffdb05974e00e02
Hamza Kabbaj <hamza.kabbaj@live.fr>
2025-05-26 13:03:43 +0100

    Navigate by default to user 12 when in root

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./pages/Home.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainLayout from "./components/ui_layouts/MainLayout/MainLayout.jsx";
 import "./styles/global.scss";
 
@@ -10,6 +10,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
+          <Route path="/" element={<Navigate to="/user/12" replace />} />
           <Route path="/user/:id" element={<Home />} />
         </Route>
       </Routes>
